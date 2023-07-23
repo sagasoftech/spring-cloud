9d004a16cc53b0a462debc425807e647cbbe85f4
Sagar Dighe <sagasoftech@gmail.com>
2023-07-23 01:18:57 -0500

    Expose spring cloud function: Supplier command to get result curl -H "Content-Type: application/json" localhost:8080/getBook

--- src/main/java/com/sagasoftech/spring/cloud/SpringCloudApplication.java
+++ src/main/java/com/sagasoftech/spring/cloud/SpringCloudApplication.java
@@ -1,10 +1,14 @@
 package com.sagasoftech.spring.cloud;
 
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.sagasoftech.spring.cloud.model.Book;
 
 @SpringBootApplication
 public class SpringCloudApplication {
@@ -17,4 +21,10 @@ public class SpringCloudApplication {
 	public Function<String, String> reverse() {
 		return (input) -> new StringBuilder(input).reverse().toString();
 	}
+	
+	@Bean
+	public Supplier<Book> getBook() {
+		return () -> new Book(101, "Core Java");
+	}
+	
 }
